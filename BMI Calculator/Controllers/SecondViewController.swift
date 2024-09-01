0b5bafe9df17644a82d98f569774ddf46ac05553
Nate Clay <claynath@gmail.com>
2024-09-01 07:49:47 -0400

    Second view controller init

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Nathaniel Clay on 8/21/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red //Can use in replacement of UIColor.red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
+    }
+    
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let selectedHeight = String(format: "%.0f", sender.value)
+        heightLabel.text = "\(selectedHeight)in"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let selectedWeight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(selectedWeight)lbs"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let BMI = (weight / pow(height, 2)) * 703
+        print(BMI)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", BMI)
+        self.present(secondVC, animated: true, completion: nil)
+    }
 }
 
